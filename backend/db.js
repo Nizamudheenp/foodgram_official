1c209117f3fd256f1b4d70abff7935c923cb0229
nizam <nizampnizam7@gmail.com>
2025-04-29 01:28:30 +0530

    implemented railway deploying fro my sql cloud

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,15 @@
-
-// db.js
 const mysql = require('mysql2');
 require('dotenv').config();
 
+const isLocalhost = ['localhost', '127.0.0.1'].includes(require('os').hostname()) 
+  || process.env.NODE_ENV !== 'production';
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: isLocalhost ? process.env.DB_HOST_LOCAL : process.env.DB_HOST,  
+  user: isLocalhost ? process.env.DB_USER_LOCAL : process.env.DB_USER,  
+  password: isLocalhost ? process.env.DB_PASSWORD_LOCAL : process.env.DB_PASSWORD, 
+  database: isLocalhost ? process.env.DB_NAME_LOCAL : process.env.DB_NAME,  
+  port: isLocalhost ? process.env.DB_PORT_LOCAL : process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const userroute = require("./routes/userroute");
 const adminroute = require("./routes/adminroute");
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(cors({
     origin: ["http://127.0.0.1:5500",
