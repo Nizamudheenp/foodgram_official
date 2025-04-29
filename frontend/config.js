239800f2da9d3365cc480ef8a5c112a781450e9a
nizam <nizampnizam7@gmail.com>
2025-04-29 23:37:40 +0530

    added railway backend url in frontend

--- frontend/config.js
+++ frontend/config.js
@@ -3,5 +3,5 @@ const isLocalhost = ['localhost', '127.0.0.1'].includes(window.location.hostname
 
 const BASE_URL = isLocalhost
   ? 'http://localhost:3000'
-  : 'https://foodgram-official.vercel.app';
+  : 'foodgramofficial-production.up.railway.app';
 
