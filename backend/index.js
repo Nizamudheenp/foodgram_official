7a332cbb2d43362bedbae95a8744103002b53cea
nizam <nizampnizam7@gmail.com>
2025-04-30 21:42:28 +0530

    changed cloud db name

--- backend/index.js
+++ backend/index.js
@@ -12,24 +12,23 @@ const port = process.env.PORT || 3000;
 app.use(cors({
     origin: ["http://127.0.0.1:5500",
         process.env.FRONTEND_URL
-        ],
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
 }))
 
-  
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
-app.use("/api/auth",authRoutes)
-app.use("/api/user",userroute)
-app.use("/api/admin",adminroute)
+app.use("/api/auth", authRoutes)
+app.use("/api/user", userroute)
+app.use("/api/admin", adminroute)
 
 app.get('/', (req, res) => {
     res.send('Foodgram backend is running!');
 });
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
 
-  
