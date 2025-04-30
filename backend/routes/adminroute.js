720602bc4313228455c04bb882e0d6c31964f7df
nizam <nizampnizam7@gmail.com>
2025-04-30 21:53:57 +0530

    env changed

--- backend/routes/adminroute.js
+++ backend/routes/adminroute.js
@@ -3,9 +3,9 @@ const { getPendingSpots, approveSpot, deleteSpot, getSpots, getApprovedSpots, ed
 const { verifyToken, verifyAdmin } = require('../middleware/authMiddleware');
 const router = express.Router();
 
-router.get("/pending-spots",verifyToken ,verifyAdmin, getPendingSpots)
-router.get("/get-spots",verifyToken ,verifyAdmin, getApprovedSpots)
-router.put("/approve-spot/:id",verifyToken,verifyAdmin, approveSpot)
+router.get("/pending-spots", verifyToken, verifyAdmin, getPendingSpots)
+router.get("/get-spots", verifyToken, verifyAdmin, getApprovedSpots)
+router.put("/approve-spot/:id", verifyToken, verifyAdmin, approveSpot)
 router.put('/edit-spot/:id', verifyToken, verifyAdmin, editSpot);
 router.delete('/delete-spot/:id', verifyToken, verifyAdmin, deleteSpot);
 
